dffed63556f13242c765799e0321a96db25302ba
marknic <marknic@microsoft.com>
2021-08-02 22:06:40 +0000

    reset everything

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+#az login
+
+$resourceGroupName = "Functions-In-A-Vnet-RG"
+$location = "eastus"
+
+if ($(az group exists --name $resourceGroupName) -eq $false) {
+    az group create -l $location -n $resourceGroupName
+}
+
+
+az deployment group create -g $resourceGroupName --template-file '.\functionapp.json' --parameters '@functionapp.parameters.json' 
